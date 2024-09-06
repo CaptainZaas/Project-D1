df4460b143ed11e5903829027aeba2a01ed8b3ae
CaptainZaas <captazazaas@gmail.com>
2024-09-06 18:57:01 +0700

    new

---
+++ pages/deboardtes.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-card
+      class="mx-auto"
+      max-width="800"
+      rounded="0"
+    >
+      <v-img
+        height="100%"
+        src="https://asset.gecdesigns.com/img/wallpapers/starry-night-sky-reflection-background-hd-wallpaper-sr10012425-1705222416280-cover.webp"
+        cover
+      >
+        <v-avatar
+          color="grey"
+          rounded="100"
+          size="300"
+        >
+          <v-img src="https://scontent.fbkk7-2.fna.fbcdn.net/v/t39.30808-6/412414744_1420456248886792_3975346427053946238_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeESkcGfyHTt_CFlUZgrp0I7gPqzm8h2CoqA-rObyHYKinwwCrjr3kKyM72F1eycD8kEVPn22IC6vOIjtXjKIbNB&_nc_ohc=K7C9y0Df4fsQ7kNvgG-XncC&_nc_ht=scontent.fbkk7-2.fna&oh=00_AYDptC5dqHTWZkw0dTAWTZ5J6Qjr4acujbKxc3nCflJ7GA&oe=66D5BBA9" cover></v-img>
+        </v-avatar>
+        <v-list-item class="text-white" tile="profile-item">
+  <v-list-item-content>
+    <v-list-item-title class="headline">
+      Mr. Warinthon Nanthaboonma (นายวรินทร นันทะบุญมา)
+    </v-list-item-title>
+    <v-list-item-subtitle class="subheading">
+        Code name : Ghost Ronin#1411
+    </v-list-item-subtitle>
+  </v-list-item-content>
+  <v-list-item-action>
+    # เกิดเมื่อวันที่: 15 ตุลาคม 2548 / 15 October 2005 
+  </v-list-item-action>
+  <v-list-item-action>
+    # สถานะ: มีเเฟนเเล้ว / I have a boyfriend.
+  </v-list-item-action>
+  <v-list-item-action>
+    # ที่อยู่บ้าน: บ้านเลขที่ 190 หมู่ 2
+  </v-list-item-action>
+  <v-list-item-action>
+    # ภูมิลำเนา: ต.โคกมั่งงอย อ.คอนสวรรค์ จ.ชัยภูมิ รหัสไปรษณีย์ 36140
+  </v-list-item-action>
+  <v-list-item-action>
+    # กำลังศึกษาอยู่ในระดับชั้น: ปวส.1 เเผนกเทคโนโลยีสารสนเทศ วิทยาลัยเทคนิคชัยภูมิ
+  </v-list-item-action>
+  <v-list-item-action>
+    # สิ่งที่ชอบ: ชอบขับรถมอไซต์ ชอบเที่ยว ชอบออกกำลังกาย ชอบเล่นเกม ชอบกิน เเละชอบนอน eiei
+  </v-list-item-action>
+</v-list-item>
+
+      </v-img>
+    </v-card>
+  </template>
+<script>
+
+</script>
